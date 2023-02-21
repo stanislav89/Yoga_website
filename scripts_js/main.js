84a7b6755652fa6a4037cbb0125c8047beea5b7e
S t a n i s l a v <dartonetorr@gmail.com>
2023-02-21 23:37:38 +0200

    js scripts with menu script

---
+++ scripts_js/main.js
@@ -0,0 +1,7 @@
+// HAMBURGER MENU
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
